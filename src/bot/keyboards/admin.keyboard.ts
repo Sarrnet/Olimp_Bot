fdd1bf2087527db9c6efe27ceb62f5a6fcf0b602
Sarnet <zaranikyaroslavma@gmail.com>
2026-05-06 17:26:10 +0300

    Delete src/bot directory

--- src/bot/keyboards/admin.keyboard.ts
+++
@@ -1,7 +0,0 @@
-import { Markup } from 'telegraf'
-
-export const adminKeyboard = Markup.keyboard([
-    ['📊 Общая статистика', '🏷 A/B Тесты'],
-    ['📢 Массовая рассылка', '📥 Экспорт пользователя'],
-    ['🏠 Главное меню'],
-]).resize()
